58c2101f2d89be87515e6450312b6962534ecc69
depp <depp@Johnny-depp-no-MacBook.local>
2016-09-20 01:14:17 +0900

    20160920

--- uniq_txt.py
+++ uniq_txt.py
@@ -1,44 +1,47 @@
 # -*- coding: utf-8 -*-
 
-import sys,re,os
+import csv
+import os
+import re
+import sys
 from collections import Counter
 from shutil import copyfile
-import csv
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     param = sys.argv
     print param
     fileName = param[1]
-    fileType = "csv" if fileName.find(".csv") else "txt"
-    print "filetype is %s" % fileType
+    fileType = 'csv' if fileName.find('.csv') else 'txt'
+    print 'filetype is %s' % fileType
     f = open(fileName, 'r')
     li = []
     for line in f:
         li.append(line)
-    overlap = [key for key,val in Counter(li).items() if val > 1]
-    print "**Overlap line list**\n------------------------"
+    overlap = [key for key, val in Counter(li).items() if val > 1]
+    print '**Overlap line list**\n------------------------'
     for i in overlap:
         print i
-    print "------------------------"
-    flg = raw_input("Are you sure you want to create a unique list to remove the above-mentioned? (y/n) > ")
-    if flg == "y":
+    print '------------------------'
+    flg = raw_input(
+        'Are you sure you want to create a unique list to remove the above-mentioned? (y/n) > ')
+    if flg == 'y':
         # ファイル名変更
         f.close
-        rename = "%s.back" % fileName
+        rename = '%s.back' % fileName
         copyfile(fileName, rename)
-        print "completed copy."
+        print 'completed copy.'
         list_uniq = []
         # 順序を保持したまま重複を削除
         # list_uniq = sorted(set(li), key=li.index)
+
         def f7(seq):
             seen = set()
             seen_add = seen.add
-            return [ x for x in seq if x not in seen and not seen_add(x)]
+            return [x for x in seq if x not in seen and not seen_add(x)]
         list_uniq = f7(li)
-        print "overlap is deleted."
-        f2 = open("%s.txt" % fileName, 'a')
-        f2.write("")
+        print 'overlap is deleted.'
+        f2 = open('%s.txt' % fileName, 'a')
+        f2.write('')
         for i in list_uniq:
             f2.writelines(i)
         # if fileType == "txt":
@@ -52,9 +55,9 @@ if __name__ == "__main__":
         #     csvWriter = csv.writer(f2)
         #     for i in list_uniq:
         #         csvWriter.writerow(i.encode('sjis'))
-        print "**** All completed ****"
+        print '**** All completed ****'
         f2.close
     else:
-        print "**** Program is ended ****"
+        print '**** Program is ended ****'
         sys.exit()
         f.close
